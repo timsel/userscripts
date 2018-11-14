6bbd3be081eb9902f1e06077882ba969e96dc157
timsel <akva.istvan@gmail.com>
2018-11-14 19:35:44 +0100

    Update bta.js

--- bta.js
+++ bta.js
@@ -1,1 +1,13 @@
-jQuery(".msg :contains(bta1)").parent().toggle();
+// ==UserScript==
+// @name         bta1 blocker :)
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://prohardver.hu/tema/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    jQuery(".msg :contains(bta1)").parent().hide();
+})();
