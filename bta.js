ef9faa54acd7ec7ec1e0c5fbcb14cdd7ef481280
timsel <akva.istvan@gmail.com>
2018-11-17 14:46:17 +0100

    Update bta.js

--- bta.js
+++ bta.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://prohardver.hu/tema/*
+// @match        https://prohardver.hu/*
 // @grant        none
 // ==/UserScript==
 
